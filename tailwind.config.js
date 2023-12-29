b5cc079e5a00eb9354aef26827e26586b5958028
Anup Kumar Maurya <62827230+Anup-maurya@users.noreply.github.com>
2023-12-29 21:49:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   important: true,
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+   
   },
   plugins: [],
 }
